fe3dc51d192043b213460d6544b202a5119f5da3
athoo <pingjing.yang@gmail.com>
2014-10-04 00:10:35 +0800

    refresh

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -2,8 +2,8 @@
 module TsvBuddy
   # @data should be a data structure that stores information
   #  from TSV or Yaml files. For example, it could be an Array of Hashes.
-  attr_accessor :data
-
+  attr_accessor :data, :header_names
+  # $header_names = []
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
@@ -14,9 +14,9 @@ module TsvBuddy
   def tsv_parser(tsv)
     sep_line = tsv.split("\n") 	# init var for each of lines seperated by '\n'.
     trans_sep = [] 	# transform each element in sep_line into an array.
-    $header_names = []
+    # $header_names = []
     sep_line.each { |x| trans_sep.push(x.split("\t")) }
-    $header_names = trans_sep[0] # Save the header for later use
+    @header_names = trans_sep[0] # Save the header for later use
     trans_sep.delete_at(0) # better for later use (ex: each..)
     yield trans_sep
   end
@@ -25,7 +25,7 @@ module TsvBuddy
     complete_array = []	 # init var for final array of hashes.
     tsv_parser(tsv) do |trans_sep|
       trans_sep.each do |arr_of_header|
-        temp_hash = Hash[$header_names.zip(arr_of_header)]
+        temp_hash = Hash[@header_names.zip(arr_of_header)]
         # zip header content with names and put into hash
         complete_array << temp_hash
       end
@@ -35,24 +35,18 @@ module TsvBuddy
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
+  # def to_tsv
+
   def to_tsv
-    stu_info = @data
-    # print stu_info
-    flag = false
-    keyarr = []
-    valuearr = []
-    wholevalue = []
-    stu_info.each do |unique|
+    flag, keyarr, valuearr, wholevalue = false, [], [], []
+    @data.each do |unique|
       unique.each do |a, b|
         keyarr.push a if flag == false
         valuearr.push b
       end
       wholevalue.push valuearr.join("\t")
-      valuearr = []
-      flag = true
+      valuearr, flag = [], true
     end
-    wholevalue.unshift(keyarr.join("\t"))
-    out = wholevalue.join("\n")
-    out << "\n"
+    wholevalue.unshift(keyarr.join("\t")).join("\n") + "\n"
   end
 end
